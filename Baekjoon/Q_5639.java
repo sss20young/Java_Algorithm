df491f7fc50efd8e145610706f7a66e17ca93869
Seoyoung Kim <43838022+sss20young@users.noreply.github.com>
2021-01-06 13:19:05 +0900

    5639번 - 이진 검색 트리

--- Baekjoon/Q_5639.java
+++ Baekjoon/Q_5639.java
@@ -1,19 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Q_5639 {
 	static int[][] array = new int[1000000][2];
-	static ArrayList<Integer> nodes_left = new ArrayList<Integer>(); // 왼쪽 트리
-	static ArrayList<Integer> nodes_right = new ArrayList<Integer>(); // 오른쪽 트리
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int root = Integer.parseInt(br.readLine()); // root node
-		nodes_left.add(root);
-		nodes_right.add(root);
 		
 		String line = null;
 		int node;
@@ -21,13 +16,7 @@ public class Q_5639 {
 		// while (!(line = br.readLine()).equals("")) { // -> 런타임 에러
 		while ((line = br.readLine()) != null && line.length() != 0) {
 			node = Integer.parseInt(line);
-			if (node < root) {
-				adjacencyMatrix(root, node);
-				nodes_left.add(node);
-			} else {
-				adjacencyMatrix(root, node);
-				nodes_right.add(node);
-			}
+			adjacencyMatrix(root, node);
 		}
 		
 		postorderTraversal(root);
