80705b25b21089ef4b3774ee679f53caf8c037c6
unknown <ksyt98@gmail.com>
2021-02-10 18:11:41 +0900

    11279번 - 최대 힙 / 우선순위 큐

---
+++ Baekjoon/Q_11279.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Q_11279 {
+    static int N, X;
+    
+        //Collections.reverseOrder());
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> o2 - o1); // 내림차순
+
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            X = Integer.parseInt(br.readLine());
+
+            if (X != 0) // X가 자연수
+                priorityQueue.offer(X);
+            else {
+                if (priorityQueue.isEmpty())
+                    sb.append(0+"\n");
+                else
+                    sb.append(priorityQueue.poll()+"\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
